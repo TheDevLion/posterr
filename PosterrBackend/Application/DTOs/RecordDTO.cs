TheDevLion/basic-x-clone

migrate project

BasicXCloneBackend/Application/DTOs/RecordDTO.cs
@@ -1,0 +1,9 @@
+﻿namespace BasicXCloneBackend.Application.DTOs
+{
+	public class RecordDTO : PostDTO
+	{
+        public bool IsPost { get; set; }
+	}
+}
+
+

BasicXCloneBackend/Domain/Interfaces/IHelperRepository.cs
@@ -1,0 +1,11 @@
+﻿using BasicXCloneBackend.Domain.Entities;
+
+namespace BasicXCloneBackend.Domain.Interfaces
+{
+	public interface IHelperRepository
+	{
+        Task<List<Record>> GetSortedPostsByPage(int page);
+        Task<List<Post>> GetSortedPostsByTrend(int page);
+    }
+}
+

BasicXCloneBackend/Domain/Interfaces/IPostRepository.cs
@@ -1,0 +1,14 @@
+﻿using BasicXCloneBackend.Domain.Entities;
+
+namespace BasicXCloneBackend.Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>> GetPosts();
+        Task<Post?> GetPostById(int idPost);
+        Task<int> CreatePost(Post newPost);
+        Task<int> CountPostsByUserAndDate(string userName, DateTime date);
+        Task<List<Post>> FilterPostsByKeywords(string keywords, int page);
+    }
+}
+

BasicXCloneBackend/Domain/Interfaces/IUserRepository.cs
@@ -1,0 +1,11 @@
+﻿using BasicXCloneBackend.Domain.Entities;
+
+namespace BasicXCloneBackend.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+        Task<bool> IsUserValid(string userName);
+    }
+}
+

BasicXCloneBackend/Infrastructure/Helper.cs
@@ -1,0 +1,20 @@
+﻿using BasicXCloneBackend.Domain.Constants;
+
+namespace BasicXCloneBackend.Infrastructure
+{
+	public static class Helper
+	{
+        public static (int, int) GetSkipAndTakeAmount(int page)
+        {
+            int firstLoadAmount = Constants.RECORDS_IN_FIRST_LOAD;
+            int normalLoadAmount = Constants.RECORDS_TO_LOAD;
+
+            int skipAmount = page == 0 ? 0 : firstLoadAmount + ((page - 1) * normalLoadAmount);
+            int takeAmount = page == 0 ? firstLoadAmount : normalLoadAmount;
+
+            return (skipAmount, takeAmount);
+        }
+    }
+}
+
+

BasicXCloneBackend/Infrastructure/Repositories/PostRepository.cs
@@ -1,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BasicXCloneBackend.Domain.Entities;
+using BasicXCloneBackend.Domain.Interfaces;
+
+namespace BasicXCloneBackend.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Post>> GetPosts()
+        {
+            return await _context.Post.ToListAsync();
+        }
+
+        public async Task<Post?> GetPostById(int idPost)
+        {
+            return await _context.Post.FirstOrDefaultAsync(p => p.Id == idPost);
+        }
+
+        public async Task<int> CreatePost(Post newPost)
+        {
+            await _context.Post.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+            return newPost.Id;
+        }
+
+        public async Task<int> CountPostsByUserAndDate(string userName, DateTime date)
+        {
+            return await _context.Post
+                .Where(p => p.Creator == userName && p.CreationDate.Date == date.Date)
+                .CountAsync();
+        }
+
+        public async Task<List<Post>> FilterPostsByKeywords(string keywords, int page)
+        {
+            int skipAmount, takeAmount;
+            (skipAmount, takeAmount) = Helper.GetSkipAndTakeAmount(page);
+
+            return await _context.Post
+                        .Where(p => p.Text.Contains(keywords))
+                        .OrderByDescending(x => x.CreationDate)
+                        .Skip(skipAmount)
+                        .Take(takeAmount)
+                        .ToListAsync();
+        }
+
+    }
+}
+

BasicXCloneBackend/BasicXCloneAPI/SwaggerExamples/CreateNewRepostRequestExample.cs
@@ -1,0 +1,18 @@
+﻿using BasicXCloneBackend.Application.RequestModels;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace BasicXCloneBackend.BasicXCloneAPI.SwaggerExamples
+{
+	public class CreateNewRepostExample
+	{
+        public class CreateNewRepostRequestExample : IExamplesProvider<CreateNewRepostRequest>
+        {
+            public CreateNewRepostRequest GetExamples()
+            {
+                return new CreateNewRepostRequest(1, "neymarjr");
+            }
+        }
+    }
+}
+
+

BasicXCloneBackend/BasicXCloneAPITests/PostTest.cs
@@ -1,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BasicXCloneBackend.Application.DTOs;
+using BasicXCloneBackend.Application.Interfaces;
+using BasicXCloneBackend.Application.Mapping;
+using BasicXCloneBackend.Application.Services;
+using BasicXCloneBackend.Domain.Entities;
+using BasicXCloneBackend.Domain.Exceptions;
+using BasicXCloneBackend.Domain.Interfaces;
+using BasicXCloneBackend.Infrastructure;
+using BasicXCloneBackend.Infrastructure.Repositories;
+
+namespace BasicXCloneAPITests;
+
+public class PostTest
+{
+    private readonly IPostService _postService;
+
+    private AppDbContext CreateInMemoryContext(string dbName)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+
+        return new AppDbContext(options);
+    }
+
+    private async Task<AppDbContext> CreateTableWithData()
+    {
+        var dbName = Guid.NewGuid().ToString();
+        var context = CreateInMemoryContext(dbName);
+
+        context.Post.Add(new Post
+        {
+            Id = 1,
+            Text = "Test post",
+            Creator = "rodrigoczleo",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 2,
+            Text = "My dream is to be world champion",
+            Creator = "neymarjr",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 3,
+            Text = "Test post 3 - Champion",
+            Creator = "johntextor",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 4,
+            Text = "Test post 4 - champion",
+            Creator = "rodrigoczleo",
+            CreationDate = DateTime.UtcNow
+        });
+
+        await context.SaveChangesAsync();
+        return context;
+    }
+
+    public PostTest()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddDbContext<AppDbContext>(options =>
+            options.UseInMemoryDatabase("basic-x-clone_Post"));
+
+        serviceCollection.AddScoped<IPostService, PostService>();
+        serviceCollection.AddScoped<IPostRepository, PostRepository>();
+        serviceCollection.AddScoped<IRepostService, RepostService>();
+        serviceCollection.AddScoped<IRepostRepository, RepostRepository>();
+        serviceCollection.AddScoped<IHelperService, HelperService>();
+        serviceCollection.AddScoped<IHelperRepository, HelperRepository>();
+
+        serviceCollection.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        serviceCollection.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        _postService = serviceProvider.GetRequiredService<IPostService>();
+    }
+
+    [Fact]
+    public async Task Should_Get_Posts()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var posts = await postRepository.GetPosts();
+
+        Assert.NotNull(posts);
+        Assert.Equal(4, posts.Count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Create_Post()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+
+        var newPost = new Post() { Id = 555, Creator = "neymarjr", CreationDate = DateTime.UtcNow, Text = "test" };
+
+        var repostId = await postRepository.CreatePost(newPost);
+
+        Assert.Equal(555, repostId);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Get_Post_By_Id()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var post = await postRepository.GetPostById(2);
+
+        Assert.NotNull(post);
+        Assert.Equal("My dream is to be world champion", post.Text);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Get_Daily_User_Posts_In_Day()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var count = await postRepository.CountPostsByUserAndDate("rodrigoczleo", DateTime.UtcNow);
+
+        Assert.Equal(2, count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Filter_Posts_By_Keyword()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var posts = await postRepository.FilterPostsByKeywords("champion", 0);
+
+        Assert.NotNull(posts);
+        Assert.Equal(2, posts.Count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Create_New_Post_On_Service()
+    {
+        var temp1 = new PostDTO() { Id = 101, Creator = "rodrigoczleo", Text = "Just one more", CreationDate = DateTime.UtcNow };
+        var newPostId = await _postService.CreatePost(temp1);
+        Assert.Equal(101, newPostId);
+
+        var temp2 = new PostDTO() { Id = 102, Creator = "rodrigoczleo", Text = "", CreationDate = DateTime.UtcNow };
+        var exception = await Assert.ThrowsAsync<NotProperContentException>(() => _postService.CreatePost(temp2));
+        Assert.IsType<NotProperContentException>(exception);
+
+        var temp3 = new PostDTO() { Id = 103, Creator = "rodrigoczleo", Text = "to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text",
+                CreationDate = DateTime.UtcNow };
+        var exception2 = await Assert.ThrowsAsync<NotProperContentException>(() => _postService.CreatePost(temp3));
+        Assert.IsType<NotProperContentException>(exception2);
+
+        for(int i=0; i<4; i++)
+        {
+            var temp = new PostDTO() { Id = 100 + 4 + i, Creator = "rodrigoczleo", Text = "Just one more", CreationDate = DateTime.UtcNow };
+            var newPostIdTemp = await _postService.CreatePost(temp);
+        }
+
+        var temp4 = new PostDTO() { Id = 110, Creator = "rodrigoczleo", Text = "another one", CreationDate = DateTime.UtcNow };
+        var exception3 = await Assert.ThrowsAsync<DailyActionsExceededException>(() => _postService.CreatePost(temp4));
+        Assert.IsType<DailyActionsExceededException>(exception3);
+    }
+
+    [Fact]
+    public async Task Should_Filter_Posts_By_Keyword_On_Service()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            var temp = new PostDTO() { Id = 120 + 4 + i, Creator = "neymarjr", Text = "Just one more", CreationDate = DateTime.UtcNow };
+            var newPostIdTemp = await _postService.CreatePost(temp);
+        }
+        var posts = await _postService.FilterPostsByKeywords("one", 0);
+        Assert.NotNull(posts);
+        Assert.Equal(4, posts.Count);
+    }
+}
+

TheDevLion/posterr

PosterrBackend/Application/DTOs/RecordDTO.cs
@@ -1,0 +1,8 @@
+﻿namespace PosterrBackend.Application.DTOs
+{
+	public class RecordDTO : PostDTO
+	{
+        public bool IsPost { get; set; }
+	}
+}
+

PosterrBackend/Domain/Interfaces/IHelperRepository.cs
@@ -1,0 +1,10 @@
+﻿using PosterrBackend.Domain.Entities;
+
+namespace PosterrBackend.Domain.Interfaces
+{
+	public interface IHelperRepository
+	{
+        Task<List<Record>> GetSortedPostsByPage(int page);
+        Task<List<Post>> GetSortedPostsByTrend(int page);
+    }
+}

PosterrBackend/Domain/Interfaces/IPostRepository.cs
@@ -1,0 +1,13 @@
+﻿using PosterrBackend.Domain.Entities;
+
+namespace PosterrBackend.Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>> GetPosts();
+        Task<Post?> GetPostById(int idPost);
+        Task<int> CreatePost(Post newPost);
+        Task<int> CountPostsByUserAndDate(string userName, DateTime date);
+        Task<List<Post>> FilterPostsByKeywords(string keywords, int page);
+    }
+}

PosterrBackend/Domain/Interfaces/IUserRepository.cs
@@ -1,0 +1,10 @@
+﻿using PosterrBackend.Domain.Entities;
+
+namespace PosterrBackend.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+        Task<bool> IsUserValid(string userName);
+    }
+}

PosterrBackend/Infrastructure/Helper.cs
@@ -1,0 +1,19 @@
+﻿using PosterrBackend.Domain.Constants;
+
+namespace PosterrBackend.Infrastructure
+{
+	public static class Helper
+	{
+        public static (int, int) GetSkipAndTakeAmount(int page)
+        {
+            int firstLoadAmount = Constants.RECORDS_IN_FIRST_LOAD;
+            int normalLoadAmount = Constants.RECORDS_TO_LOAD;
+
+            int skipAmount = page == 0 ? 0 : firstLoadAmount + ((page - 1) * normalLoadAmount);
+            int takeAmount = page == 0 ? firstLoadAmount : normalLoadAmount;
+
+            return (skipAmount, takeAmount);
+        }
+    }
+}
+

PosterrBackend/Infrastructure/Repositories/PostRepository.cs
@@ -1,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PosterrBackend.Domain.Entities;
+using PosterrBackend.Domain.Interfaces;
+
+namespace PosterrBackend.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Post>> GetPosts()
+        {
+            return await _context.Post.ToListAsync();
+        }
+
+        public async Task<Post?> GetPostById(int idPost)
+        {
+            return await _context.Post.FirstOrDefaultAsync(p => p.Id == idPost);
+        }
+
+        public async Task<int> CreatePost(Post newPost)
+        {
+            await _context.Post.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+            return newPost.Id;
+        }
+
+        public async Task<int> CountPostsByUserAndDate(string userName, DateTime date)
+        {
+            return await _context.Post
+                .Where(p => p.Creator == userName && p.CreationDate.Date == date.Date)
+                .CountAsync();
+        }
+
+        public async Task<List<Post>> FilterPostsByKeywords(string keywords, int page)
+        {
+            int skipAmount, takeAmount;
+            (skipAmount, takeAmount) = Helper.GetSkipAndTakeAmount(page);
+
+            return await _context.Post
+                        .Where(p => p.Text.Contains(keywords))
+                        .OrderByDescending(x => x.CreationDate)
+                        .Skip(skipAmount)
+                        .Take(takeAmount)
+                        .ToListAsync();
+        }
+
+    }
+}

PosterrBackend/PosterrAPI/SwaggerExamples/CreateNewRepostRequestExample.cs
@@ -1,0 +1,17 @@
+﻿using PosterrBackend.Application.RequestModels;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace PosterrBackend.PosterrAPI.SwaggerExamples
+{
+	public class CreateNewRepostExample
+	{
+        public class CreateNewRepostRequestExample : IExamplesProvider<CreateNewRepostRequest>
+        {
+            public CreateNewRepostRequest GetExamples()
+            {
+                return new CreateNewRepostRequest(1, "neymarjr");
+            }
+        }
+    }
+}
+

PosterrBackend/PosterrAPITests/PostTest.cs
@@ -1,0 +1,193 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PosterrBackend.Application.DTOs;
+using PosterrBackend.Application.Interfaces;
+using PosterrBackend.Application.Mapping;
+using PosterrBackend.Application.Services;
+using PosterrBackend.Domain.Entities;
+using PosterrBackend.Domain.Exceptions;
+using PosterrBackend.Domain.Interfaces;
+using PosterrBackend.Infrastructure;
+using PosterrBackend.Infrastructure.Repositories;
+
+namespace PosterrAPITests;
+
+public class PostTest
+{
+    private readonly IPostService _postService;
+
+    private AppDbContext CreateInMemoryContext(string dbName)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+
+        return new AppDbContext(options);
+    }
+
+    private async Task<AppDbContext> CreateTableWithData()
+    {
+        var dbName = Guid.NewGuid().ToString();
+        var context = CreateInMemoryContext(dbName);
+
+        context.Post.Add(new Post
+        {
+            Id = 1,
+            Text = "Test post",
+            Creator = "rodrigoczleo",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 2,
+            Text = "My dream is to be world champion",
+            Creator = "neymarjr",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 3,
+            Text = "Test post 3 - Champion",
+            Creator = "johntextor",
+            CreationDate = DateTime.UtcNow
+        });
+        context.Post.Add(new Post
+        {
+            Id = 4,
+            Text = "Test post 4 - champion",
+            Creator = "rodrigoczleo",
+            CreationDate = DateTime.UtcNow
+        });
+
+        await context.SaveChangesAsync();
+        return context;
+    }
+
+    public PostTest()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddDbContext<AppDbContext>(options =>
+            options.UseInMemoryDatabase("Posterr_Post"));
+
+        serviceCollection.AddScoped<IPostService, PostService>();
+        serviceCollection.AddScoped<IPostRepository, PostRepository>();
+        serviceCollection.AddScoped<IRepostService, RepostService>();
+        serviceCollection.AddScoped<IRepostRepository, RepostRepository>();
+        serviceCollection.AddScoped<IHelperService, HelperService>();
+        serviceCollection.AddScoped<IHelperRepository, HelperRepository>();
+
+        serviceCollection.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        serviceCollection.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        _postService = serviceProvider.GetRequiredService<IPostService>();
+    }
+
+    [Fact]
+    public async Task Should_Get_Posts()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var posts = await postRepository.GetPosts();
+
+        Assert.NotNull(posts);
+        Assert.Equal(4, posts.Count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Create_Post()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+
+        var newPost = new Post() { Id = 555, Creator = "neymarjr", CreationDate = DateTime.UtcNow, Text = "test" };
+
+        var repostId = await postRepository.CreatePost(newPost);
+
+        Assert.Equal(555, repostId);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Get_Post_By_Id()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var post = await postRepository.GetPostById(2);
+
+        Assert.NotNull(post);
+        Assert.Equal("My dream is to be world champion", post.Text);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Get_Daily_User_Posts_In_Day()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var count = await postRepository.CountPostsByUserAndDate("rodrigoczleo", DateTime.UtcNow);
+
+        Assert.Equal(2, count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Filter_Posts_By_Keyword()
+    {
+        var context = await CreateTableWithData();
+        var postRepository = new PostRepository(context);
+        var posts = await postRepository.FilterPostsByKeywords("champion", 0);
+
+        Assert.NotNull(posts);
+        Assert.Equal(2, posts.Count);
+
+        context.Dispose();
+    }
+
+    [Fact]
+    public async Task Should_Create_New_Post_On_Service()
+    {
+        var temp1 = new PostDTO() { Id = 101, Creator = "rodrigoczleo", Text = "Just one more", CreationDate = DateTime.UtcNow };
+        var newPostId = await _postService.CreatePost(temp1);
+        Assert.Equal(101, newPostId);
+
+        var temp2 = new PostDTO() { Id = 102, Creator = "rodrigoczleo", Text = "", CreationDate = DateTime.UtcNow };
+        var exception = await Assert.ThrowsAsync<NotProperContentException>(() => _postService.CreatePost(temp2));
+        Assert.IsType<NotProperContentException>(exception);
+
+        var temp3 = new PostDTO() { Id = 103, Creator = "rodrigoczleo", Text = "to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text, to long text",
+                CreationDate = DateTime.UtcNow };
+        var exception2 = await Assert.ThrowsAsync<NotProperContentException>(() => _postService.CreatePost(temp3));
+        Assert.IsType<NotProperContentException>(exception2);
+
+        for(int i=0; i<4; i++)
+        {
+            var temp = new PostDTO() { Id = 100 + 4 + i, Creator = "rodrigoczleo", Text = "Just one more", CreationDate = DateTime.UtcNow };
+            var newPostIdTemp = await _postService.CreatePost(temp);
+        }
+
+        var temp4 = new PostDTO() { Id = 110, Creator = "rodrigoczleo", Text = "another one", CreationDate = DateTime.UtcNow };
+        var exception3 = await Assert.ThrowsAsync<DailyActionsExceededException>(() => _postService.CreatePost(temp4));
+        Assert.IsType<DailyActionsExceededException>(exception3);
+    }
+
+    [Fact]
+    public async Task Should_Filter_Posts_By_Keyword_On_Service()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            var temp = new PostDTO() { Id = 120 + 4 + i, Creator = "neymarjr", Text = "Just one more", CreationDate = DateTime.UtcNow };
+            var newPostIdTemp = await _postService.CreatePost(temp);
+        }
+        var posts = await _postService.FilterPostsByKeywords("one", 0);
+        Assert.NotNull(posts);
+        Assert.Equal(4, posts.Count);
+    }
+}